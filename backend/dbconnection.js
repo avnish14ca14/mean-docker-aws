0b7c65a1f26f162e11d4992df990a843046aa0f5
avnishkaushik111 <avnishkaushik111@gmail.com>
2020-01-31 11:14:21 +0530

    db error fixed

--- backend/dbconnection.js
+++ backend/dbconnection.js
@@ -2,14 +2,14 @@ const mysql = require('mysql');
 
 var conn = mysql.createConnection({
     host: 'avnishkumarkaushik1c.mylabserver.com',
+    database: 'dockerlabdb',
     user: 'root',
     password: 'Kaushik1234#'
 });
 
 conn.connect(function(err){
-    if(!err){
-        console.log('Connected');
-    }else{
-        console.log(err);
-    }
+    if(err) throw err;
+    console.log('Connected');
 });
+
+exports.conn = conn;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 var dbconnection = require('./dbconnection');
+var conn = dbconnection.conn;
 
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Docker App');
+    conn.query('select * from userdetails', function(err, result){
+        if(err) throw err;
+        res.send(result);
+    });
 });
 
 app.listen(4000, () => {
